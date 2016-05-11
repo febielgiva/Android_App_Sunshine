0909451495fc6d433fde6a0c52fadcc28732380b
febielgiva <idoffebi@gmail.com>
2016-05-10 20:47:25 -0700

    intial commit for milestone1

---
+++ app/src/main/java/com/example/android/sunshine/app/Detailed_ActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.android.sunshine.app;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Detailed_ActivityFragment extends Fragment {
+
+    public Detailed_ActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detailed_, container, false);
+    }
+}
